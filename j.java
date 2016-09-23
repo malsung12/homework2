06b5e52b97fee3e91128022d0cc0a00bc26ee25f
박성원 <malsung12@gmail.com>
2016-09-23 19:30:37 +0900

    j파일 코딩

---
+++ j.java
@@ -0,0 +1,24 @@
+class j{
+	int age = 0;
+	public static void main(String[] arg){
+		new j().statr();
+	
+	}
+	
+	void start(){
+		Scanner new  = Scanner(System.in);
+
+		System.out.print("나이를 입력:");
+		int year = s.nextln();
+
+		this.age = 2016 - year + 1;
+
+
+	}
+	void print(){
+		if(age < 20)
+		System.out.println("미성년자 입니다");
+		else
+		System.out.println("미성년자가 아닙니다.");
+	}
+}
